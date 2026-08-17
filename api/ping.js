c69c3da7b653d1f6da15cb668e4125f6f1ab165e
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-08-17 17:44:33 -0400

    Add temporary Action diagnostic ping endpoint

---
+++ api/ping.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ ok: false, error: 'Method not allowed' });
+  }
+
+  return res.status(200).json({ ok: true, source: 'action' });
+}
