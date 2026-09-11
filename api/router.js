8ccaac5bc43ab430e74774c1e925078dd63ae7e2
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-09-11 13:31:42 -0300

    Allow commercial operation universe on data loader router

--- api/router.js
+++ api/router.js
@@ -18,6 +18,7 @@ const TENANTS = {
       'forum_canonicalizer_v01',
       'forum_commercial_bridge_v01',
       'commercial_operation_master_v01',
+      'commercial_operation_universe_v01',
       'refresh_vehiculo_canonico_v01',
       'patch_inventario_modelo',
       'rvm_market_history_v01',
